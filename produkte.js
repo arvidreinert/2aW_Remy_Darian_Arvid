53109686456880442e473111cf6b309a73a27e7e
Arvid Reinert <168847324+arvidreinert@users.noreply.github.com>
2026-02-17 20:26:43 +0100

    Grosses Warenkorb Update und ein kleiner Test (iA)

---
+++ produkte.js
@@ -0,0 +1,5 @@
+const produkte = [
+  { name: "Kopfhoerer", preis: 67.5 , bild: "product1.jpg"},
+  { name: "Bratpfanne", preis: 25 , bild: "product2.jpg"},
+  { name: "Kontroller fuer N64", preis: 79 , bild: "product3.jpg"}
+];
