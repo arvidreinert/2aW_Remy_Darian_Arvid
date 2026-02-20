45525a5f6e3e010701effca92af9f8a7cfc57847
Arvid Reinert <168847324+arvidreinert@users.noreply.github.com>
2026-02-20 10:29:06 +0100

    produkteseite update

--- javascript/produkte.js
+++ javascript/produkte.js
@@ -1,5 +1,9 @@
 const produkte = [
-  { name: "Kopfhoerer", preis: 67.5 , bild: "product1.jpg"},
+  { name: "Kopfhoerer", preis: 67.67 , bild: "product1.jpg"},
   { name: "Bratpfanne", preis: 25 , bild: "product2.jpg"},
-  { name: "Kontroller fuer N64", preis: 79 , bild: "product3.jpg"}
+  { name: "Kontroller fuer N64", preis: 79.86 , bild: "product3.jpg"},
+  { name: "Fernseher", preis: 499.99 , bild: "product4.jpg"},
+  { name: "Gaming Stuhl", preis: 99.99 , bild: "product5.jpg"},
+  { name: "Mountainbike", preis: 249.5 , bild: "product6.jpg"}
+  
 ];
