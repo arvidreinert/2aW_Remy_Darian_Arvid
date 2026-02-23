216901d0c784996b2c5df44d69aaf52f34e5a7b1
Arvid Reinert <168847324+arvidreinert@users.noreply.github.com>
2026-02-23 16:15:12 +0100

    eingabeformular auf Kontaktseite

---
+++ javascript/email.js
@@ -0,0 +1,18 @@
+emailjs.init("eNcYqnLmmdUyRMhG-");
+
+document
+  .getElementById("contactform")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    emailjs
+      .sendForm("service_rnagssp", "template_18sz2tk", this)
+      .then(
+        function () {
+          alert("Mail erfolgreich gesendet!");
+        },
+        function (error) {
+          alert("Fehler: " + JSON.stringify(error));
+        }
+      );
+  });
